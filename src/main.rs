f2e51a1196b020cbb3ebfcdb9bb6ca9b118ddf42
bfresnel <5629642+bfresnel@users.noreply.github.com>
2023-05-16 01:32:13 +0200

    Added logs

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,30 @@
+use log::info;
+use rand::{thread_rng, Rng};
 use std::thread;
 use std::time::{Duration, Instant};
-use rand::{Rng, thread_rng};
 
 fn main() {
+    simple_logger::SimpleLogger::new().env().init().unwrap();
+
     let mut random_thread = thread_rng();
     let thread_pool_limit = random_thread.gen_range(1..11);
 
-    println!("Number of activated thread(s) : {}", thread_pool_limit);
+    info!("Number of activated thread(s) : {}", thread_pool_limit);
 
     for i in 0..thread_pool_limit {
-        thread::spawn(move||{
+        thread::spawn(move || {
             let now = Instant::now();
             for _x in 0..101 {
                 let mut random_ms = thread_rng();
                 let random_value = random_ms.gen_range(0..101);
-                //println!("[THREAD-{}] Loading... {}%", i, x);
+                //debug!("[THREAD-{}] Loading... {}%", i, x);
                 thread::sleep(Duration::from_millis(random_value));
-            };
-            println!("THREAD {} finished in {} ms", i + 1, now.elapsed().as_millis());
+            }
+            info!(
+                "THREAD {} finished in {} ms",
+                i + 1,
+                now.elapsed().as_millis()
+            );
         });
     }
 
